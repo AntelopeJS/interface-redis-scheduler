3d95c0ff99fb4b5cc1ae1ed3582a81068ed0f3fb
Antony Rizzitelli <upd4ting@gmail.com>
2026-03-25 16:20:29 +0100

    fix: update @antelopejs/redis test dependency to 1.0.1

--- src/antelope.test.ts
+++ src/antelope.test.ts
@@ -8,7 +8,7 @@ export default defineConfig({
       source: {
         type: "package",
         package: "@antelopejs/redis",
-        version: "1.0.0",
+        version: "1.0.1",
       },
       config: { useMock: true },
     },
